5fdfd2bd08f381569e7b265c51620013d6e46e90
hamrod <moy_97@hotmail.fr>
2021-02-08 21:17:33 +0100

    conditions de défaite changé

--- src/fr/istic/ia/tp1/EnglishDraughts.java
+++ src/fr/istic/ia/tp1/EnglishDraughts.java
@@ -449,11 +449,23 @@ public class EnglishDraughts extends Game {
      */
     @Override
     public PlayerId winner() {
+
+        if (possibleMoves().size() == 0) {
+            switch(player()) {
+                case ONE -> {
+                    return PlayerId.TWO;
+                }
+                case TWO -> {
+                    return PlayerId.ONE;
+                }
+            }
+        }
+
         // return the winner ID if possible
         if (board.getWhitePawns().isEmpty()) return PlayerId.TWO;
         else if (board.getBlackPawns().isEmpty()) return PlayerId.ONE;
             // return PlayerId.NONE if the game is null
-        else if ((nbKingMovesWithoutCapture >= 25) || possibleMoves().size() == 0) return PlayerId.NONE;
+        else if (nbKingMovesWithoutCapture >= 25) return PlayerId.NONE;
             // Return null is the game has not ended yet
         else return null;
     }
